f49dc3dc4750fe654e85c1a83deb6bbb29b0907b
Matt McManus <mattmcmanus@gmail.com>
2010-12-12 11:16:56 -0500

    New server nav bar. SQ client side object improvements

--- public/javascripts/statusQuo.js
+++ public/javascripts/statusQuo.js
@@ -6,19 +6,42 @@
 if(typeof window.statusQuo === "undefined") {
   var statusQuo = function (params) {
     this.servers = null;
+    this.checkOnLoad = true || params.checkOnLoad;
     
     this.getConfig();
   };
   
   statusQuo.prototype = {
+    setupPage: function() {
+      this.bindButtons();
+      if (this.checkOnLoad) {
+        var context = this;
+        $('.sites').each(function(site){
+          var id = $(this).attr('id');
+          context.check(id);
+        })
+      }
+    },
+    
     getConfig: function(){
       var context = this;
       $.getJSON('/getConfig', function(data) {
         context.servers = data;
       });
     },
+    
+    bindButtons: function() {
+      //Bind server refresh action
+      $('.server a.refresh').click(function(){
+        $(this).parent().next().children().each(function(){
+          sq.check($(this).attr('id'))
+        });
+      });
+    },
+    
     check: function(id){
-      var context = $('#'+id);
+      var context = $("#"+id);
+      $(context).addClass('checking');
       $.ajax({ url:'/check/'+id, context:context, success: function(data){
         $(this).addClass('s'+data.statusCode).removeClass('checking').find('.message').text(data.message)
         if(data.statusCode == 500)
@@ -28,12 +51,13 @@ if(typeof window.statusQuo === "undefined") {
   }
 };
 
+sq = new statusQuo({
+  "checkOnLoad":false
+});
 
-sq = new statusQuo({});
 
 $(document).ready(function() {
+  sq.setupPage();
   
-  $('.site').each(function(index){
-    sq.check($(this).attr('id'));
-  })
+  
 });
