806c59294ae89ec80aec4a537fe17c6ad20dca6f
Matt McManus <mattmcmanus@gmail.com>
2011-07-05 15:06:51 -0400

    Another test. Huzzah. #3

--- test/app.test.js
+++ test/app.test.js
@@ -10,29 +10,33 @@ require('../app').boot(function(server) {
   app = server;
   
   module.exports = {
-    'Get the home page and confirm a 200' : function(beforeExit) {
+    'Get the home page and confirm a 200' : function() {
       assert.response(app, { url: '/', method: 'GET'}, function(res) {
         res.statusCode.should.equal(200)
-        beforeExit(function(){});
       })
     },
     
-    'Get an invalid page as an anonymous user and get a 404': function(beforeExit) {
+    'Get an invalid page as an anonymous user and get a 404': function() {
       assert.response(app, { url: '/this-is-an-invalid-page', method: 'GET'}, function(res) {
         res.statusCode.should.equal(404)
-        beforeExit(function(){});
       })
     },
     
-    'Get the create server page as an anonymous user and get a forbidden': function(beforeExit) {
+    'Get the create server page as an anonymous user and get a forbidden': function() {
       assert.response(app, { url: '/server/new', method: 'GET'}, function(res) {
         res.statusCode.should.equal(403)
-        beforeExit(function(){});
+      })
+    },
+    
+    'Get the server lookup page as an anonymous user and get a 200': function() {
+      assert.response(app, { url: '/server/lookup/127.0.0.1', method: 'GET'}, function(res) {
+        res.statusCode.should.equal(200)
       })
     }
   }
 });
-  
+
+// Since the tests don't seem to stop on their own...
 setTimeout(function(){
   process.exit()
 }, 5000);
